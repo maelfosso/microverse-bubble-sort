1eb151457f94e420ca406a2cb1051e536d12be96
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-04-05 11:32:44 +0100

    Using block's return

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -16,10 +16,9 @@ end
 def bubble_sort_by(array)
   for i in 0..array.size - 2
     for j in 0..array.size - 2
-      yield(array[j], array[j + 1])
+      cmp = yield(array[j], array[j + 1])
 
-      cmp = array[j] <=> array[j + 1]
-      if (cmp < 0)
+      if (cmp > 0)
         tmp = array[j]
         array[j] = array[j + 1]
         array[j + 1] = tmp
