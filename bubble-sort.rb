86a33acdce48ae41c87f7a31083f3915d07fd432
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-04-05 10:53:10 +0100

    Following instructions. Write it as a function

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,15 +1,14 @@
-#!/usr/bin/env ruby
 
-array = ARGV.map { |e| e.to_i  }
-
-for i in 0..array.size - 2
-  for j in i+1..array.size - 1
-    if (array[j - 1] > array[j])
-      tmp = array[j]
-      array[j] = array[j - 1]
-      array[j - 1] = tmp
+def bubble_sort(array)
+  for i in 0..array.size - 2
+    for j in i+1..array.size - 1
+      if (array[j - 1] > array[j])
+        tmp = array[j]
+        array[j] = array[j - 1]
+        array[j - 1] = tmp
+      end
     end
   end
-end
 
-p array
+  return array
+end
