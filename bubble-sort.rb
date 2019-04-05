20db30fb3b8728f1ea51fb237907428ad1703bcb
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-04-05 11:25:45 +0100

    Functions created. bubble_sort_by added

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,11 +1,28 @@
 
 def bubble_sort(array)
   for i in 0..array.size - 2
-    for j in i+1..array.size - 1
-      if (array[j - 1] > array[j])
+    for j in 0..array.size - 2
+      if (array[j] > array[j + 1])
         tmp = array[j]
-        array[j] = array[j - 1]
-        array[j - 1] = tmp
+        array[j] = array[j + 1]
+        array[j + 1] = tmp
+      end
+    end
+  end
+
+  return array
+end
+
+def bubble_sort_by(array)
+  for i in 0..array.size - 2
+    for j in 0..array.size - 2
+      yield(array[j], array[j + 1])
+
+      cmp = array[j] <=> array[j + 1]
+      if (cmp < 0)
+        tmp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = tmp
       end
     end
   end
